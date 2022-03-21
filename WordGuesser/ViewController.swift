56ad5ec60621ebff2cd47876818612560fb3bc33
Shane <302752144@student.rocmondriaan.nl>
2022-03-21 14:31:09 +0100

    style is finished, modes are finished.

--- WordGuesser/ViewController.swift
+++ WordGuesser/ViewController.swift
@@ -7,14 +7,76 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var welcomeMessage: UITextView!
+    @IBOutlet weak var redButton: UIButton!
+    @IBOutlet weak var greenButton: UIButton!
+    @IBOutlet weak var yellowButton: UIButton!
+    @IBOutlet weak var blueButton: UIButton!
+    @IBOutlet weak var buttonsView: UIView!
+    @IBOutlet weak var question: UILabel!
+    @IBOutlet weak var modeLabel: UILabel!
+    @IBOutlet weak var flashButton: UIButton!
+    @IBOutlet weak var fillButton: UIButton!
+    
+    var color = "test";
+    
+    var currentElementIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    var questions: [String] = []
+    var answers: [String] = []
+    
+    @IBAction func redClick(_ sender: UIButton) {
+        color = "red";
+        performSegue(withIdentifier: "showDetail", sender: nil)
+    }
+    @IBAction func greenClick(_ sender: UIButton) {
+        color = "green";
+        performSegue(withIdentifier: "showDetail", sender: nil)
+    }
+    @IBAction func yellowClick(_ sender: Any) {
+        color = "yellow";
+        performSegue(withIdentifier: "showDetail", sender: nil)
+    }
+    @IBAction func blueClick(_ sender: Any) {
+        color = "blue";
+        performSegue(withIdentifier: "showDetail", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        if segue.identifier == "showDetail",
+           let ViewController = segue.destination as? ViewController {
+            ViewController.color = color;
+        }
+    }
+     
+    
+    @IBAction func fill(_ sender: UIButton) {
+        question.isHidden = false;
+        question.text = "Hoe groot is de zon?";
+        modeLabel.isHidden = false;
+        modeLabel.text = "Fill";
+        flashButton.isHidden = true;
+        fillButton.isHidden = true;
+        
+        
+    }
+    @IBAction func Flashcard(_ sender: Any) {
+        question.isHidden = false;
+        question.text = "Hoe groot is de zon?";
+        modeLabel.isHidden = false;
+        modeLabel.text = "Flashcard";
+        flashButton.isHidden = true;
+        fillButton.isHidden = true;
+    }
+    
+ 
+    
+    
 }
 
